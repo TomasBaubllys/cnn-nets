e825e6f604cf3209e1870a636a1de6e165306f91
tomas <tomas.baublys32@gmail.com>
2026-04-16 15:33:29 +0300

    updated residual block

---
+++ conv2dnet/conv2dMain.py
@@ -0,0 +1,25 @@
+import torch
+from conv2dnet.conv2dNet import Conv2dNetStd, Conv2dNetRes
+from conv2dnet.rockPaperScissorsDataset import RockPaperScissorsDataset
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from torch import optim
+import matplotlib.pyplot as plt
+import numpy as np
+from utils.nnutils import test, train
+
+if __name__ == "__main__":
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    dataset_train = RockPaperScissorsDataset("conv2dnet/rockpaperscissors/train", transform)
+    dataset_val = RockPaperScissorsDataset("conv2dnet/rockpaperscissors/validation", transform)
+    dataset_test = RockPaperScissorsDataset("conv2dnet/rockpaperscissors/test", transform)
+
+    modelSmall = Conv2dNetRes()
+    train(modelSmall, dataset_train, dataset_val, 10, model_name="rps_small")
+    test(modelSmall, dataset_val, "rps_small_model_weights.pth")
---
+++ conv2dnet/conv2dNet.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Conv2dNetStd(nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.cnn_layers = nn.Sequential (
+			nn.Conv2d(3, 4, kernel_size=3, stride=1, padding=1),
+			nn.AvgPool2d(kernel_size=4, stride=1, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(4, 4, kernel_size=3, stride=2, padding=1),
+			nn.AvgPool2d(kernel_size=4, stride=2, padding=1),
+			nn.Conv2d(4, 4, kernel_size=3, stride=2, padding=0),
+			nn.BatchNorm2d(4),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=2, stride=2)
+		)
+
+		self.fc = nn.Sequential(
+			nn.Linear(676, 3)
+			#nn.Linear(50176, 3)
+		)
+
+	def forward(self, x):
+		x = self.cnn_layers(x)
+		x = torch.flatten(x, 1)
+		x = self.fc(x)
+		return x
+	
+class ResBlock(nn.Module):
+	def __init__(self, channels=4):
+		super().__init__()
+
+		self.conv1 = nn.Conv2d(channels, channels, 3, stride=1, padding=1, bias=False)
+		self.bn1 = nn.BatchNorm2d(channels)
+
+		self.conv2 = nn.Conv2d(channels, channels, 3, stride=1, padding=1, bias=False)
+		self.bn2 = nn.BatchNorm2d(channels)
+
+	def forward(self, x):
+		identity = x
+
+		out = self.conv1(x)
+		out = self.bn1(out)
+		out = F.relu(out, inplace=True)
+
+		out = self.conv2(out)
+		out = self.bn2(out)
+
+		out = out + identity
+		out = F.relu(out, inplace=True)
+
+		return out
+
+class Conv2dNetRes(nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.res_block = ResBlock(16)
+		self.prep_layer = nn.Sequential(
+			nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+			nn.AvgPool2d(kernel_size=4, stride=1, padding=1),
+			nn.ReLU(inplace=True)
+		)
+		self.last_layer = nn.AdaptiveAvgPool2d(4)
+
+		self.fc = nn.Sequential(
+			nn.Linear(256, 3)
+			#nn.Linear(50176, 3)
+		)
+
+	def forward(self, x):
+		x = self.prep_layer(x)
+		x = self.res_block(x)
+		x = self.last_layer(x)
+		x = torch.flatten(x, 1)
+		x = self.fc(x)
+		return x
+
+if __name__ == "__main__":
+	model = Conv2dNetStd()
+	print(model)
+	
+	dummy_input = torch.rand(1, 3, 224, 224)
+	print(dummy_input)
+	print(model(dummy_input))
