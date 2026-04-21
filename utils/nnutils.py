4635f9d86c21e8509c9f8eb9bdbdb0248d5613b6
tomas <tomas.baublys32@gmail.com>
2026-04-22 00:30:49 +0300

    added plotting loss

--- utils/nnutils.py
+++ utils/nnutils.py
@@ -94,7 +94,7 @@ def test_single_pass(model, dataloader, device, criterion=None):
 
 # plots an array of losses provided over epochs
 def plot_loss(loss_hists, labels, epochs, figname):
-    x = np.arange(0, epochs + 1)
+    x = np.arange(1, epochs + 1)
     for (loss_train, loss_val), label in zip(loss_hists, labels):
         plt.plot(x, loss_train, label=f"{label} train loss")
         plt.plot(x, loss_val, label=f"{label} val loss")
