d8889fba3a6785c75b8247dd998c5e7e6bd5d3d4
tomas <tomas.baublys32@gmail.com>
2026-04-22 15:23:10 +0300

    added plotting loss

--- conv2dnet/conv2dNet.py
+++ conv2dnet/conv2dNet.py
@@ -77,9 +77,12 @@ class Conv2dNetRes(nn.Module):
 		x = self.fc(x)
 		return x
 
-class Conv2dNetResBig(nn.Module, dropout=0, prep_pool=nn.AvgPool2d(kernel_size=4, stride=1, padding=1)):
+class Conv2dNetResBig(nn.Module, dropout=0, prep_pool=None):
 	def __init__(self):
 		super().__init__()
+		if not prep_pool:
+			prep_pool=nn.AvgPool2d(kernel_size=4, stride=1, padding=1)
+
 		self.res_block1 = ResBlock(8)
 		self.prep_layer = nn.Sequential(
 			nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1),
